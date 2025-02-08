ddebbb4965fd55cd87569e18d0b04901901c8a2b
Luca Barone <baro.luc@gmail.com>
2025-02-08 19:35:18 +0100

    Init

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import panda from "@pandacss/eslint-plugin";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    ignores: ["**/*.d.ts", "styled-system"],
+    plugins: {
+      "@pandacss": panda,
+    },
+    rules: {
+      // You can also use the recommended rules
+      ...panda.configs.recommended.rules,
+    },
+  }
+);
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { css } from "../styled-system/css";
+export function App() {
+  return (
+    <h1
+      className={css({
+        color: "red.500",
+      })}
+    >
+      hi
+    </h1>
+  );
+}
