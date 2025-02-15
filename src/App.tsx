6a6493f7332c94f4801cfd63f309aef2c4815cfe
Luca Barone <baro.luc@gmail.com>
2025-02-15 02:08:41 +0100

    update dependencies to link panda-lib and refactor App component to use new imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-import {css} from '../styled-system/css'
+import {Button} from 'panda-lib'
+import {css} from 'panda-lib/css'
+import {hstack} from 'panda-lib/patterns'
 
 export function App() {
-  return <h1 className={css({textStyle: '3xl', fontWeight: 'bold'})}>Hello</h1>
+  return (
+    <div className={hstack({p: '6'})}>
+      <h1 className={css({textStyle: '3xl', fontWeight: 'bold'})}>Hello</h1>
+      <Button>Click me</Button>
+      <span className={css({textDecoration: 'underline'})}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum,
+        eveniet? Reiciendis quo eum voluptatem a! Nemo ad, doloribus, facere
+        harum provident nam voluptates architecto sint aperiam blanditiis
+        aliquid, quisquam iure.
+      </span>
+    </div>
+  )
 }
